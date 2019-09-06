22091ce577ea0d68df2bbbe07b5eacd5072e837f
Ajay Kumar <42745121+ajaykumar1018@users.noreply.github.com>
2019-09-06 18:36:43 +0530

    Update master_final.ino

--- master_final.ino
+++ master_final.ino
@@ -9,32 +9,22 @@
 #define l3 D2
 #define l4 D3
 
-// You should get Auth Token in the Blynk App.
-// Go to the Project Settings (nut icon).
+char auth[] = "ULbUhXP9jViMQ-4uO-UUey6iGexd5G2q"; //auth token of blynk
 
-char auth[] = "ULbUhXP9jViMQ-4uO-UUey6iGexd5G2q";
-
-// Your WiFi credentials.
-// Set password to "" for open networks.
 byte ledPin = 2;
-char ssid[] = "Next Tech Lab";               // SSID of your home WiFi
-char pass[] = "78959248";               // password of your home WiFi
-WiFiServer server(80);    
+char ssid[] = "Next Tech Lab";  
+char pass[] = "78959248";               
+WiFiServer server(80);    //port
 
-// This function will be called every time Slider Widget
-// in Blynk app writes values to the Virtual Pin 1
-
-IPAddress ip(192, 168, 0, 100);            // IP address of the server
-IPAddress gateway(192,168,0,1);           // gateway of your network
-IPAddress subnet(255,255,255,0);  
+IPAddress ip(192, 168, 0, 100);  //ip address of master and slave should be same
+IPAddress gateway(192,168,0,1);  //gateway should be same      
+IPAddress subnet(255,255,255,0);  //subnet should be same
 BLYNK_WRITE(V1)
 {
   int pinValue = param.asInt(); // assigning incoming value from pin V1 to a variable
-  // You can also use:
-  // String i = param.asStr();
-  // double d = param.asDouble();
   Serial.print("V1 Slider value is: ");
   Serial.println(pinValue);
+  
   if (pinValue == 1){
   digitalWrite(l1,LOW);
   digitalWrite(l2,HIGH);
@@ -43,27 +33,24 @@ BLYNK_WRITE(V1)
    WiFiClient client = server.available();
   if (client) {
     if (client.connected()) {
-      digitalWrite(ledPin, LOW);  // to show the communication only (inverted logic)
+      digitalWrite(ledPin, LOW);  
       Serial.println(".");
-     // String request = client.readStringUntil('\r');    // receives the message from the client
-     // Serial.print("From client: "); Serial.println(request);
+
       client.flush();
       client.println("FORWARD\r"); // sends the answer to the client
       digitalWrite(ledPin, HIGH);
     }
     client.stop();                // tarminates the connection with the client
   }
-  }
-  
+  } 
 }
+
 BLYNK_WRITE(V2)
 {
-  int pinValue = param.asInt(); // assigning incoming value from pin V1 to a variable
-  // You can also use:
-  // String i = param.asStr();
-  // double d = param.asDouble();
+  int pinValue = param.asInt(); // assigning incoming value from pin V2 to a variable
   Serial.print("V2 Slider value is: ");
   Serial.println(pinValue);
+  
   if (pinValue == 1){
   digitalWrite(l1,HIGH);
   digitalWrite(l2,LOW);
@@ -72,26 +59,23 @@ BLYNK_WRITE(V2)
    WiFiClient client = server.available();
   if (client) {
     if (client.connected()) {
-      digitalWrite(ledPin, LOW);  // to show the communication only (inverted logic)
+      digitalWrite(ledPin, LOW); 
       Serial.println(".");
-     // String request = client.readStringUntil('\r');    // receives the message from the client
-     // Serial.print("From client: "); Serial.println(request);
       client.flush();
       client.println("BACKWARD\r"); // sends the answer to the client
       digitalWrite(ledPin, HIGH);
     }
-    client.stop();                // tarminates the connection with the client
+    client.stop();         
   }
   }
 }
+
 BLYNK_WRITE(V3)
 {
-  int pinValue = param.asInt(); // assigning incoming value from pin V1 to a variable
-  // You can also use:
-  // String i = param.asStr();
-  // double d = param.asDouble();
+  int pinValue = param.asInt(); // assigning incoming value from pin V3 to a variable
   Serial.print("V3 Slider value is: ");
   Serial.println(pinValue);
+  
   if (pinValue == 1){
   digitalWrite(l1,HIGH);
   digitalWrite(l2,HIGH);
@@ -100,24 +84,21 @@ BLYNK_WRITE(V3)
    WiFiClient client = server.available();
   if (client) {
     if (client.connected()) {
-      digitalWrite(ledPin, LOW);  // to show the communication only (inverted logic)
+      digitalWrite(ledPin, LOW);  
       Serial.println(".");
-     // String request = client.readStringUntil('\r');    // receives the message from the client
-     // Serial.print("From client: "); Serial.println(request);
       client.flush();
       client.println("LEFT\r"); // sends the answer to the client
       digitalWrite(ledPin, HIGH);
     }
-    client.stop();                // tarminates the connection with the client
+    client.stop();                  
   }
   }
 }
+
 BLYNK_WRITE(V4)
 {
-  int pinValue = param.asInt(); // assigning incoming value from pin V1 to a variable
-  // You can also use:
-  // String i = param.asStr();
-  // double d = param.asDouble();
+  int pinValue = param.asInt(); // assigning incoming value from pin V4 to a variable
+
   Serial.print("V4 Slider value is: ");
   Serial.println(pinValue);
   if (pinValue == 1){
@@ -128,26 +109,23 @@ BLYNK_WRITE(V4)
    WiFiClient client = server.available();
   if (client) {
     if (client.connected()) {
-      digitalWrite(ledPin, LOW);  // to show the communication only (inverted logic)
+      digitalWrite(ledPin, LOW); 
       Serial.println(".");
-     // String request = client.readStringUntil('\r');    // receives the message from the client
-     // Serial.print("From client: "); Serial.println(request);
       client.flush();
       client.println("RIGHT\r"); // sends the answer to the client
       digitalWrite(ledPin, HIGH);
     }
-    client.stop();                // tarminates the connection with the client
+    client.stop();          
   }
   }
 }
+
 BLYNK_WRITE(V5)
 {
-  int pinValue = param.asInt(); // assigning incoming value from pin V1 to a variable
-  // You can also use:
-  // String i = param.asStr();
-  // double d = param.asDouble();
+  int pinValue = param.asInt(); // assigning incoming value from pin V5 to a variable
   Serial.print("V5 Slider value is: ");
   Serial.println(pinValue);
+  
   if (pinValue == 1){
   digitalWrite(l1,HIGH);
   digitalWrite(l2,HIGH);
@@ -156,15 +134,13 @@ BLYNK_WRITE(V5)
    WiFiClient client = server.available();
   if (client) {
     if (client.connected()) {
-      digitalWrite(ledPin, LOW);  // to show the communication only (inverted logic)
+      digitalWrite(ledPin, LOW);  
       Serial.println(".");
-     // String request = client.readStringUntil('\r');    // receives the message from the client
-     // Serial.print("From client: "); Serial.println(request);
       client.flush();
       client.println("STOP\r"); // sends the answer to the client
       digitalWrite(ledPin, HIGH);
     }
-    client.stop();                // tarminates the connection with the client
+    client.stop();     
   }
   }
 }
@@ -172,47 +148,28 @@ BLYNK_WRITE(V5)
 void setup()
 {
   pinMode(l1, OUTPUT);
-pinMode(l2, OUTPUT);
-pinMode(l3, OUTPUT);
-pinMode(l4, OUTPUT);
-  // Debug console
+  pinMode(l2, OUTPUT);
+  pinMode(l3, OUTPUT);
+  pinMode(l4, OUTPUT);
   Serial.begin(115200);
 
   Blynk.begin(auth, ssid, pass);
-  // You can also specify server:
-  //Blynk.begin(auth, ssid, pass, "blynk-cloud.com", 80);
-  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
-   // Serial.begin(115200);                   // only for debug
-  WiFi.config(ip, gateway, subnet);       // forces to use the fix IP
-  WiFi.begin(ssid, pass);                 // connects to the WiFi router
+  WiFi.config(ip, gateway, subnet);      
+  WiFi.begin(ssid, pass);         
   while (WiFi.status() != WL_CONNECTED) {
     Serial.print(".");
     delay(500);
-  }
-  server.begin();                         // starts the server
+    }
+  server.begin();                        
   pinMode(ledPin, OUTPUT);
 }
 
 void loop()
 {
   Blynk.run();
-   BLYNK_WRITE(V1);
+  BLYNK_WRITE(V1);
   BLYNK_WRITE(V2);
   BLYNK_WRITE(V3);
   BLYNK_WRITE(V4);
   BLYNK_WRITE(V5);
- /* WiFiClient client = server.available();
-  if (client) {
-    if (client.connected()) {
-      digitalWrite(ledPin, LOW);  // to show the communication only (inverted logic)
-      Serial.println(".");
-     // String request = client.readStringUntil('\r');    // receives the message from the client
-     // Serial.print("From client: "); Serial.println(request);
-      client.flush();
-      client.println("FORWARD\r"); // sends the answer to the client
-      digitalWrite(ledPin, HIGH);
-    }
-    client.stop();                // tarminates the connection with the client
-  }*/
-
 }
